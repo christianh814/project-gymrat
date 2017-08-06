d91cdae724664a7cdcf69f41f0a8c547bd2ffe4b
Christian Hernandez <christian.hernandez@yahoo.com>
2017-08-06 12:34:41 -0700

    totally more readable

--- index.php
+++ index.php
@@ -55,21 +55,10 @@
 
       <hr>
 
-    </div><!--/.container-->
-
       <footer>
         <p>&copy; 2016 uFit, Inc.</p>
       </footer>
 
+    </div><!--/.container-->
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="js/jquery.min.js"><\/script>')</script>
-    <script src="js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="js/ie10-viewport-bug-workaround.js"></script>
-    <script src="js/offcanvas.js"></script>
-  </body>
-</html>
+<?php include "includes/pages/footer.php"; ?>
