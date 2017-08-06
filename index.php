41650e4985c701f11a4ff0ade68fe90a1d832c39
Christian Hernandez <christian.hernandez@yahoo.com>
2017-08-06 12:13:26 -0700

    arrrggghhhhh fixed!

--- index.php
+++ index.php
@@ -1,25 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-    <link rel="icon" href="favicon.ico">
-
-    <title>Off Canvas Template for Bootstrap</title>
-
-    <!-- Custom styles for this template -->
-    <link href="css/offcanvas.css" rel="stylesheet">
-
-  </head>
-
+<?php include "includes/pages/header.php"; ?>
   <body>
     <nav class="navbar navbar-fixed-top navbar-inverse">
       <div class="container">
